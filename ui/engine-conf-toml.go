6c9b865365dcb7faec3d9c4891c66206c207edc6
むずでょ <muzudho1@gmail.com>
2021-02-23 13:11:27 +0900

    外部プロセスから実行すると nil になるからコメントアウト

--- ui/engine-conf-toml.go
+++ ui/engine-conf-toml.go
@@ -4,7 +4,6 @@ import (
 	"io/ioutil"
 
 	e "github.com/muzudho/kifuwarabe-gtp/entities"
-	u "github.com/muzudho/kifuwarabe-gtp/usecases"
 	"github.com/pelletier/go-toml"
 )
 
@@ -17,7 +16,7 @@ func LoadEngineConf(path string) (*e.EngineConf, error) {
 		return nil, err
 	}
 
-	debugPrintToml(fileData)
+	// debugPrintToml(fileData)
 
 	// Toml解析
 	binary := []byte(string(fileData))
@@ -27,27 +26,28 @@ func LoadEngineConf(path string) (*e.EngineConf, error) {
 	return config, nil
 }
 
-func debugPrintToml(fileData []byte) {
-	// u.G.Log.Trace("...Engine content=%s", string(fileData))
+// 外部プロセスとして呼び出される場合、まだロガーの設定が終わってないかも知れません。
+// func debugPrintToml(fileData []byte) {
+// 	// u.G.Log.Trace("...Engine content=%s", string(fileData))
 
-	// Toml解析
-	tomlTree, err := toml.Load(string(fileData))
-	if err != nil {
-		panic(err)
-	}
-	u.G.Log.Trace("...Engine Input:\n")
-	u.G.Log.Trace("...Engine Engine.Komi=%f\n", tomlTree.Get("Engine.Komi").(float64))
-	u.G.Log.Trace("...Engine Engine.BoardSize=%d\n", tomlTree.Get("Engine.BoardSize").(int64))
-	u.G.Log.Trace("...Engine Engine.MaxMoves=%d\n", tomlTree.Get("Engine.MaxMoves").(int64))
-	u.G.Log.Trace("...Engine Engine.BoardData=%s\n", tomlTree.Get("Engine.BoardData").(string))
-}
-func debugPrintConfig(config e.EngineConf) {
-	u.G.Log.Trace("...Engine Memory:\n")
-	u.G.Log.Trace("...Engine Profile.Name=%s\n", config.Profile.Name)
-	u.G.Log.Trace("...Engine Profile.Pass=%s\n", config.Profile.Pass)
-	u.G.Log.Trace("...Engine Engine.Komi=%f\n", config.Engine.Komi)
-	u.G.Log.Trace("...Engine Engine.BoardSize=%d\n", config.Engine.BoardSize)
-	u.G.Log.Trace("...Engine Engine.MaxMoves=%d\n", config.Engine.MaxMoves)
-	u.G.Log.Trace("...Engine Engine.MaxMoves=%s\n", config.Engine.BoardData)
-	u.G.Log.Trace("...Engine Engine.SentinelBoardMax()=%d\n", config.SentinelBoardMax())
-}
+// 	// Toml解析
+// 	tomlTree, err := toml.Load(string(fileData))
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	u.G.Log.Trace("...Engine Input:\n")
+// 	u.G.Log.Trace("...Engine Engine.Komi=%f\n", tomlTree.Get("Engine.Komi").(float64))
+// 	u.G.Log.Trace("...Engine Engine.BoardSize=%d\n", tomlTree.Get("Engine.BoardSize").(int64))
+// 	u.G.Log.Trace("...Engine Engine.MaxMoves=%d\n", tomlTree.Get("Engine.MaxMoves").(int64))
+// 	u.G.Log.Trace("...Engine Engine.BoardData=%s\n", tomlTree.Get("Engine.BoardData").(string))
+// }
+// func debugPrintConfig(config e.EngineConf) {
+// 	u.G.Log.Trace("...Engine Memory:\n")
+// 	u.G.Log.Trace("...Engine Profile.Name=%s\n", config.Profile.Name)
+// 	u.G.Log.Trace("...Engine Profile.Pass=%s\n", config.Profile.Pass)
+// 	u.G.Log.Trace("...Engine Engine.Komi=%f\n", config.Engine.Komi)
+// 	u.G.Log.Trace("...Engine Engine.BoardSize=%d\n", config.Engine.BoardSize)
+// 	u.G.Log.Trace("...Engine Engine.MaxMoves=%d\n", config.Engine.MaxMoves)
+// 	u.G.Log.Trace("...Engine Engine.MaxMoves=%s\n", config.Engine.BoardData)
+// 	u.G.Log.Trace("...Engine Engine.SentinelBoardMax()=%d\n", config.SentinelBoardMax())
+// }
